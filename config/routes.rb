2accea59591a6683b45e19b73230df6a6763a286
Isaac Falkenstine <isaac@falkenstine.com>
2019-04-12 13:22:53 -0600

    Make sessions controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  get '/login', to: "sessions#new"
+  post '/login', to: "sessions#create"
+
   get '/register', to: "users#new"
   resources 'users'
   get '/dashboard', to: "users#show", as: "dashboard"
