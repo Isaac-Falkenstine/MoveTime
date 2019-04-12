1e1fce68bce06d93e389f2fd755e83c6cf1d69ec
Isaac Falkenstine <isaac@falkenstine.com>
2019-04-12 12:43:21 -0600

    Make user register page

--- spec/features/user_spec.rb
+++ spec/features/user_spec.rb
@@ -8,8 +8,7 @@ describe 'As a visitor' do
     visit '/register'
 
     fill_in 'user[email]', with: "amy@amy.com"
-    fill_in 'user[first_name]', with: "Abby"
-    fill_in 'user[last_name]', with: "Smith"
+    fill_in 'user[name]', with: "Abby"
     fill_in 'user[password]', with: password
     fill_in 'user[password_confirmation]', with: password
 
@@ -26,8 +25,7 @@ describe 'As a visitor' do
     visit '/register'
 
     fill_in 'user[email]', with: "amy@amy.com"
-    fill_in 'user[first_name]', with: "Abby"
-    fill_in 'user[last_name]', with: "Smith"
+    fill_in 'user[name]', with: "Abby"
     fill_in 'user[password]', with: password
     fill_in 'user[password_confirmation]', with: password
 
