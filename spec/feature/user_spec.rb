b392badef121725c94945e5e4b0c21dcf76893a5
Isaac Falkenstine <isaac@falkenstine.com>
2019-04-12 12:25:32 -0600

    Added sad path testing

--- spec/feature/user_spec.rb
+++ spec/feature/user_spec.rb
@@ -17,4 +17,24 @@ describe 'As a visitor' do
 
     expect(page).to have_content("Registered as Abby Smith")
   end
+  
+  it 'I cannot register with existing email' do
+
+    password = "password"
+    user_1 = create(:user, email: "amy@amy.com", password: password)
+
+    visit '/register'
+
+    fill_in 'user[email]', with: "amy@amy.com"
+    fill_in 'user[first_name]', with: "Abby"
+    fill_in 'user[last_name]', with: "Smith"
+    fill_in 'user[password]', with: password
+    fill_in 'user[password_confirmation]', with: password
+
+    within(".login_submit") do
+      click_on "Register"
+    end
+
+    expect(page).to have_content("Please check your email and password information")
+  end
 end
