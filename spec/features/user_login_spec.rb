b1d49b5cf5f5ab15424ad96476945948fdbc1f7a
Isaac Falkenstine <isaac@falkenstine.com>
2019-04-12 13:29:40 -0600

    Add sad path test for logging in

--- spec/features/user_login_spec.rb
+++ spec/features/user_login_spec.rb
@@ -2,17 +2,27 @@ require "rails_helper"
 
 describe 'As a visitor' do
   it 'I can login if I already have a account' do
-
-    password = "password"
-    user_1 = create(:user, email: "email@email.com", password: password)
+    user_1 = create(:user, email: "email@email.com", password: "password")
 
     visit '/login'
 
     fill_in 'session[email]', with: "email@email.com"
-    fill_in 'session[password]', with: password
+    fill_in 'session[password]', with: "password"
 
     click_on "Log In"
 
     expect(page).to have_content("Logged in as #{user_1.name}")
+  end
+
+  it 'I cant login if I dont have a account' do
+
+    visit '/login'
+
+    fill_in 'session[email]', with: "email@email.com"
+    fill_in 'session[password]', with: "password"
+
+    click_on "Log In"
+
+    expect(page).to have_content("Looks like your email or password is invalid")
   end
 end
