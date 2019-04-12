c099f3df06d8af14cf191b61bef8facef182a938
Isaac Falkenstine <isaac@falkenstine.com>
2019-04-12 13:09:32 -0600

    Add sad path for user create method

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,12 +7,22 @@ class UsersController < ApplicationController
     if user_params[:password] == user_params[:password_confirmation]
       user = User.create(user_params)
       if user.save
+        session[:user_id] = user.id
+        flash[:notice] = "Registered as #{user.name}"
+        redirect_to dashboard_path
       else
+        flash[:notice] = "A account already exist with that email"
+        redirect_to dashboard_path
       end
     else
+      flash[:notice] = "Passwords did not match"
+      redirect_to dashboard_path
     end
   end
 
+  def show
+
+  end
 
   private
   def user_params
