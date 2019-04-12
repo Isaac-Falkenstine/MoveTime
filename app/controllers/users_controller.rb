fa661e0490686badef1947f9d61c66e4e8773680
Isaac Falkenstine <isaac@falkenstine.com>
2019-04-12 13:09:10 -0600

    Make create method for users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,21 @@
 class UsersController < ApplicationController
-
   def new
     @user = User.new
+  end
+
+  def create
+    if user_params[:password] == user_params[:password_confirmation]
+      user = User.create(user_params)
+      if user.save
+      else
+      end
+    else
+    end
+  end
+
+
+  private
+  def user_params
+    params.require(:user).permit(:email, :name, :password, :password_confirmation)
   end
 end
