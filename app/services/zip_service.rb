3e2694a58710721b2d6364cb939b72039e07ab12
Isaac Falkenstine <isaac@falkenstine.com>
2019-04-16 12:44:14 -0600

    Add responce and coords to service

--- app/services/zip_service.rb
+++ app/services/zip_service.rb
@@ -10,4 +10,12 @@ class ZipService
     end
   end
 
+  def response
+    url = "json?address=#{@location}&key=#{ENV['GOOGLE_API_KEY']}"
+    conn.get(url)
+  end
+
+  def coords
+    JSON.parse(response.body, symbolize_names: true)[:results][0][:geometry][:location]
+  end
 end
