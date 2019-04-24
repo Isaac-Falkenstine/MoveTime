e551c5f94b030b76fb17ba1a236a198ce508bd39
Isaac Falkenstine <isaac@falkenstine.com>
2019-04-23 22:58:25 -0600

    Adds living price test

--- spec/features/search_bar_spec.rb
+++ spec/features/search_bar_spec.rb
@@ -1,7 +1,7 @@
 require "rails_helper"
 
 describe "a user visits the dashboard page" do
-  it "can enter the zipcode and get back some info" do
+  it "can enter the zipcode and get back the county" do
     user = create(:user)
     allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
 
@@ -10,5 +10,16 @@ describe "a user visits the dashboard page" do
     click_on "Search"
 
     expect(page).to have_content "Littleton"
+  end
+
+  it "can enter the zipcode and get back some info" do
+    user = create(:user)
+    allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
+
+    visit dashboard_path
+    fill_in :search, with: "80124"
+    click_on "Search"
+
+    expect(page).to have_content "Living price: "
   end
 end
